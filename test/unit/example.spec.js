8781f0deda6c868dcc810d77fdcf8976d04abe88
Michael Perez <perez.michael.d@gmail.com>
2017-06-15 14:34:06 -0700

    Add example tests for karma and protractor to test that runners are set up properly

---
+++ test/unit/example.spec.js
@@ -0,0 +1,9 @@
+describe('An example test suite', function() {
+  beforeEach(function() {});
+  afterEach(function() {});
+
+  it('should pass canary test', function() {
+    expect(true).to.be.true;
+  });
+
+});
