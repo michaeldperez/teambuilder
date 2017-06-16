ca77e8ecb107d01a429f7e84683b125ead8bedf9
Michael Perez <perez.michael.d@gmail.com>
2017-06-15 17:05:54 -0700

    Update example spec to test file using es6

--- test/unit/example.spec.js
+++ test/unit/example.spec.js
@@ -1,9 +1,11 @@
+import add from '../../client/example';
+
 describe('An example test suite', function() {
   beforeEach(function() {});
   afterEach(function() {});
 
   it('should pass canary test', function() {
-    expect(true).to.be.true;
+    expect(add(1,2)).to.equal(3);
   });
 
 });
