736bee547ad92b5015534bb65c673bba0edb3fb1
Michael Perez <perez.michael.d@gmail.com>
2017-06-17 18:31:16 -0700

    Update rollup config to get api to work

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,7 @@
-import resolve from 'rollup-plugin-node-resolve';
-import babel   from 'rollup-plugin-babel';
 
 export default {
-  entry: 'client/index.js',
-  format: 'iife',
-  moduleName: 'app',
-  dest: 'dist/bundle.js',
-  sourceMap: true,
-  plugins: [
-    resolve({
-      customResolveOptions: {
-        moduleDirectory: 'node_modules'
-      }
-    }),
-    babel({
-      exclude: 'node_modules/**'
-    })
-  ],
-  external: []
+  entry: 'server/index.js',
+  dest: 'dist/server/index.js',
+  format: 'cjs',
+  plugins: []
 };
-
