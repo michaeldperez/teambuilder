365e4254f092a043b7ac9f75a872d176d40d5e56
Michael Perez <perez.michael.d@gmail.com>
2017-06-17 18:37:10 -0700

    Add api routes

---
+++ server/routes/teams.js
@@ -0,0 +1,58 @@
+import { Router } from 'express';
+
+const router     = Router()
+    , collection = 'teams';
+
+export default function(team) {
+  router.get('/', (req, res) => {
+    team.all(collection)
+        .then(teams => {
+          res.send(teams);
+        })
+        .catch(err => {
+          res.status(500)
+             .send(err.message);
+        });
+  });
+
+  router.get('/:id', (req, res) => {
+    const id         = req.params.id;
+
+    team.get(collection, id)
+        .then(team => {
+          res.send(team);
+        })
+        .catch(err => {
+          res.status(500)
+             .send(err.message);
+        })
+  });
+
+  router.post('/', (req, res) => {
+    const teamToAdd  = req.body;
+
+    team.add(collection, teamToAdd)
+        .then(() => {
+          res.send('team added');
+        })
+        .catch(err => {
+          res.status(500)
+             .send(err.message);
+        });
+  });
+
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+
+    team.delete(collection, id)
+        .then(() => {
+          res.send('team deleted');
+        })
+        .catch(err => {
+          res.status(500)
+             .send(err.message);
+        });
+  });
+
+  return router;
+};
