295c731302a26ce68b9f2f25c0822b3b2b203cfc
Michael Perez <perez.michael.d@gmail.com>
2017-06-19 21:40:16 -0700

    Include create component

--- client/index.js
+++ client/index.js
@@ -4,11 +4,17 @@ import uiRouterConfig  from './uiRouterConfig';
 import TeamsController from './components/teams/teams.controller';
 import TeamsComponent  from './components/teams/teams.component';
 import TeamsService    from './components/teams/teams.service';
+import CreateController  from './components/create/create.controller';
+import CreateComponent   from './components/create/create.component';
+import CreateService     from './components/create/create.service';
 
 const app = angular.module('teambuilder', [uiRouter])
                    .controller('TeamsController', TeamsController)
+                   .controller('CreateController', CreateController)
                    .component('teams', TeamsComponent)
+                   .component('create', CreateComponent)
                    .service('TeamsService', TeamsService)
+                   .service('CreateService', CreateService)
                    .config(uiRouterConfig);
 
 export default app;
