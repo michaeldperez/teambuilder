d6907eff1555df0a08890f14241835873dc1b669
Michael Perez <perez.michael.d@gmail.com>
2017-06-20 19:55:22 -0700

    Use mongodb-uri to connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 import path       from 'path';
 import express    from 'express';
 import bodyParser from 'body-parser';
+import config     from 'config';
+import mongodbUri from 'mongodb-uri';
 import Teams      from './routes/teams';
 import Team       from './models/team';
 import Database   from './db/db';
 
 const db = new Database();
 const app = express();
+const connectionString = mongodbUri.format(config.get('mongodb'));
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '..')));
@@ -14,7 +17,7 @@ app.use(express.static(path.join(__dirname, '..')));
 let team
   , teams;
 
-db.connect('mongodb://localhost/teambuilder')
+db.connect(connectionString)
   .then(() => {
     team = new Team(db);
     teams = Teams(team);
