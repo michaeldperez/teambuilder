df4e78fe391e0e1ea856447972bb63d6ec01b4ee
Michael Perez <perez.michael.d@gmail.com>
2017-06-20 18:29:37 -0700

    Import team and member components

--- client/index.js
+++ client/index.js
@@ -1,17 +1,23 @@
-import angular         from 'angular';
-import uiRouter        from 'angular-ui-router';
-import uiRouterConfig  from './uiRouterConfig';
-import TeamsController from './components/teams/teams.controller';
-import TeamsComponent  from './components/teams/teams.component';
-import TeamsService    from './components/teams/teams.service';
+import angular           from 'angular';
+import uiRouter          from 'angular-ui-router';
+import uiRouterConfig    from './uiRouterConfig';
+import TeamsController   from './components/teams/teams.controller';
+import TeamsComponent    from './components/teams/teams.component';
+import TeamsService      from './components/teams/teams.service';
+import TeamController    from './components/team/team.controller';
+import TeamComponent     from './components/team/team.component';
+import MemberComponent   from './components/member/member.component';
 import CreateController  from './components/create/create.controller';
 import CreateComponent   from './components/create/create.component';
 import CreateService     from './components/create/create.service';
 
 const app = angular.module('teambuilder', [uiRouter])
                    .controller('TeamsController', TeamsController)
+                   .controller('TeamController', TeamController)
                    .controller('CreateController', CreateController)
                    .component('teams', TeamsComponent)
+                   .component('team', TeamComponent)
+                   .component('member', MemberComponent)
                    .component('create', CreateComponent)
                    .service('TeamsService', TeamsService)
                    .service('CreateService', CreateService)
